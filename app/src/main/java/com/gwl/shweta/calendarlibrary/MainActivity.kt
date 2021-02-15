48bde4befa55c7912f4325c583305d4474a123a4
Shweta Jain <shweta.jain@galaxyweblinks.in>
2021-02-15 17:58:47 +0530

    1. Not able to select current date. 2. Not able to select any date if calendarView.isDisableNextfutureDate(false) 3. Provide method for custom event drawable. 4. Rename CalendarBuilder to CalendarConfig 5. Remove shweta from package

--- app/src/main/java/com/gwl/shweta/calendarlibrary/MainActivity.kt
+++ app/src/main/java/com/gwl/shweta/calendarlibrary/MainActivity.kt
@@ -1,27 +1,29 @@
 package com.gwl.shweta.calendarlibrary
 
-import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import com.gwl.shweta.calendar.CalendarView
-import com.gwl.shweta.calendar.CalendarView.CalendarBuilder
+import com.gwl.calendar.CalendarView
+import com.gwl.calendar.CalendarView.CalendarConfig
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val calendarView = findViewById<CalendarView>(R.id.calendarView)
-        CalendarBuilder(this, calendarView)
-            .setDatesColor(ContextCompat.getColor(this,R.color.dark_gray))
-            .setDaysColor(ContextCompat.getColor(this,R.color.blue))
-            .setDisabledDatesColor(ContextCompat.getColor(this,R.color.light_gray))
-            .setMonthHeaderColor(ContextCompat.getColor(this,R.color.blue))
-            .setSelectedDatesColor(ContextCompat.getColor(this,R.color.white))
-            .build()
+      /*  ContextCompat.getDrawable(this,R.drawable.rectangle)?.let {
+            CalendarConfig(this, calendarView)
+                .setDatesColor(ContextCompat.getColor(this, R.color.dark_gray))
+                .setDaysColor(ContextCompat.getColor(this, R.color.blue))
+                .setDisabledDatesColor(ContextCompat.getColor(this, R.color.light_gray))
+                .setMonthHeaderColor(ContextCompat.getColor(this, R.color.blue))
+                .setSelectedDatesColor(ContextCompat.getColor(this, R.color.white))
+                .setImage(it)
+                .build()
+        }*/
         calendarView.isDisableNextfutureDate(true)
         calendarView.updateCalendar()
         findViewById<Button>(R.id.search_close_btn).setOnClickListener(View.OnClickListener {
--- calendarlibrary/src/main/java/com/gwl/calendar/CalendarView.kt
+++ calendarlibrary/src/main/java/com/gwl/calendar/CalendarView.kt
@@ -1,8 +1,10 @@
-package com.gwl.shweta.calendar
+package com.gwl.calendar
 
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Typeface
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +14,7 @@ import androidx.core.content.ContextCompat
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
+
 
 class CalendarView : LinearLayout {
 
@@ -23,6 +26,7 @@ class CalendarView : LinearLayout {
     private var colorDates: Int = 0
     private var disableColorDates: Int = 0
     private var selectedColorDates: Int = 0
+    private var img: Drawable? = null
     private var selectedDates: ArrayList<Date> = ArrayList()
 
     // current displayed month
@@ -104,7 +108,15 @@ class CalendarView : LinearLayout {
                 R.styleable.CalendarView_selectedColorDates,
                 ContextCompat.getColor(context, R.color.white)
             )
+            img = ta.getDrawable(R.styleable.CalendarView_img)
             if (dateFormat == null) dateFormat = DATE_FORMAT
+            if (img == null) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    img = resources.getDrawable(R.drawable.reminder, null)
+                } else {
+                    img = resources.getDrawable(R.drawable.reminder)
+                }
+            }
         } finally {
             ta.recycle()
         }
@@ -160,15 +172,27 @@ class CalendarView : LinearLayout {
         grid?.also {
             it.onItemClickListener =
                 AdapterView.OnItemClickListener { view, cell, position, _ -> // handle long-press
-                    val date = view.getItemAtPosition(position) as Date
-                    val today = Date()
+                    var date = view.getItemAtPosition(position) as Date
+                    val cal: Calendar = Calendar.getInstance()
+                    cal.time = date
+                    cal.set(Calendar.HOUR_OF_DAY, 0)
+                    cal.set(Calendar.MINUTE, 0)
+                    cal.set(Calendar.SECOND, 0)
+                    cal.set(Calendar.MILLISECOND, 0)
+                    date = cal.time
+                    var today = Date()
+                    cal.time = today
+                    cal.set(Calendar.HOUR_OF_DAY, 0)
+                    cal.set(Calendar.MINUTE, 0)
+                    cal.set(Calendar.SECOND, 0)
+                    cal.set(Calendar.MILLISECOND, 0)
+                    today = cal.time
                     if (selectedDates.contains(date)) {
                         selectedDates.remove(date)
-                    } else if (isDisableNextMonthDate && date.after(today)) {
+                    } else if (!isDisableNextMonthDate || date.after(today) || date.equals(today)) {
                         selectedDates.add(date)
                     }
                     calendarAdapter?.notifyDataSetChanged()
-                    true
                 }
         }
     }
@@ -180,7 +204,7 @@ class CalendarView : LinearLayout {
         eventsList = events
     }
 
-    fun  getSelectedDates(): ArrayList<Date> {
+    fun getSelectedDates(): ArrayList<Date> {
         return selectedDates
     }
 
@@ -208,6 +232,10 @@ class CalendarView : LinearLayout {
 
         // move calendar backwards to the beginning of the week
         calendar.add(Calendar.DAY_OF_MONTH, -monthBeginningCell)
+        calendar.set(Calendar.HOUR_OF_DAY, 0)
+        calendar.set(Calendar.MINUTE, 0)
+        calendar.set(Calendar.SECOND, 0)
+        calendar.set(Calendar.MILLISECOND, 0)
 
         // fill cells
         while (cells.size < DAYS_COUNT) {
@@ -279,36 +307,30 @@ class CalendarView : LinearLayout {
 
                 // if this day has an event, specify event image
                 itemTextView.setBackgroundResource(0)
-                if (eventDays != null) {
-                    for (eventDate in eventDays) {
-                        val eventDaysCalender = DateUtil.getCanlenderFordate(eventDate)
-                        var eventMonth = eventDaysCalender.get(Calendar.MONTH)
-                        eventMonth++
+                for (eventDate in eventDays) {
+                    val eventDaysCalender = DateUtil.getCanlenderFordate(eventDate)
+                    var eventMonth = eventDaysCalender.get(Calendar.MONTH)
+                    eventMonth++
 
-                        if (eventDaysCalender.get(Calendar.DAY_OF_MONTH) == day && eventMonth == month && eventDaysCalender.get(
-                                Calendar.YEAR
-                            ) == year
-                        ) {
-                            eventHighLight.visibility = View.VISIBLE
-                            eventHighLight.setImageResource(R.drawable.circle_dot)
-                        }
-
+                    if (eventDaysCalender.get(Calendar.DAY_OF_MONTH) == day && eventMonth == month && eventDaysCalender.get(
+                            Calendar.YEAR
+                        ) == year
+                    ) {
+                        eventHighLight.visibility = View.VISIBLE
+                        eventHighLight.setImageResource(R.drawable.circle_dot)
                     }
-                }
-                val daysCalender = DateUtil.getCanlenderFordate(date)
 
+                }
                 textView.setTypeface(null, Typeface.NORMAL)
                 textView.setTextColor(disableColorDates)
-
-
                 if (month == visibleMonth && year == visibleYear) {
-
                     textView.setTextColor(colorDates)
                     if (day == presentDay && month == presentMonth && year == presentYear) {
 
                         textView.setTypeface(null, Typeface.BOLD)
                         // itemTextView.setTextColor(ContextCompat.getColor(context, R.color.blue))
-                        textView.setBackgroundResource(R.drawable.reminder)
+
+                        textView.background = img
                     } else if (date.before(today)) {
 /*
                         textView.setTextColor(ContextCompat.getColor(context, R.color.border_color))
@@ -346,10 +368,10 @@ class CalendarView : LinearLayout {
                 // set text
                 textView.text = mCalender.get(Calendar.DATE).toString()
                 if (selectedDates.contains(date)) {
-                    showView?.setBackgroundResource(R.drawable.custom_circle)
+                    showView.setBackgroundResource(R.drawable.custom_circle)
                     textView.setTextColor(selectedColorDates)
                 } else {
-                    showView?.setBackgroundResource(0)
+                    showView.setBackgroundResource(0)
                 }
 
             }
@@ -382,7 +404,7 @@ class CalendarView : LinearLayout {
         private const val DATE_FORMAT = "MMM yyyy"
     }
 
-    public class CalendarBuilder {
+    public class CalendarConfig {
         private var calendarView: CalendarView? = null;
 
         constructor(context: Context, calendarView: CalendarView) {
@@ -393,38 +415,44 @@ class CalendarView : LinearLayout {
             calendarView.colorDates = ContextCompat.getColor(context, R.color.white)
             calendarView.disableColorDates = ContextCompat.getColor(context, R.color.white)
             calendarView.selectedColorDates = ContextCompat.getColor(context, R.color.white)
+            calendarView.img = ContextCompat.getDrawable(context, R.drawable.reminder)
         }
 
         constructor(context: Context) : this(context, CalendarView(context)) {
         }
 
-        fun setDateFormat(dateFormat: String): CalendarBuilder {
+        fun setDateFormat(dateFormat: String): CalendarConfig {
             calendarView?.dateFormat = dateFormat
             return this;
         }
 
-        fun setMonthHeaderColor(monthColor: Int): CalendarBuilder {
+        fun setMonthHeaderColor(monthColor: Int): CalendarConfig {
             calendarView?.monthColor = monthColor
             return this
         }
 
-        fun setDaysColor(daysColor: Int): CalendarBuilder {
+        fun setDaysColor(daysColor: Int): CalendarConfig {
             calendarView?.colorDays = daysColor
             return this
         }
 
-        fun setDatesColor(datesColor: Int): CalendarBuilder {
+        fun setDatesColor(datesColor: Int): CalendarConfig {
             calendarView?.colorDates = datesColor
             return this
         }
 
-        fun setDisabledDatesColor(disabledColorDates: Int): CalendarBuilder {
+        fun setDisabledDatesColor(disabledColorDates: Int): CalendarConfig {
             calendarView?.disableColorDates = disabledColorDates
             return this
         }
 
-        fun setSelectedDatesColor(selectedDatesColor: Int): CalendarBuilder {
+        fun setSelectedDatesColor(selectedDatesColor: Int): CalendarConfig {
             calendarView?.selectedColorDates = selectedDatesColor
+            return this
+        }
+
+        fun setImage(image: Drawable): CalendarConfig {
+            calendarView?.img = image
             return this
         }
 
